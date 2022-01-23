0996788e685b55617cf99c7969647b5d87aa4fd6
Kenny Pflug <kenny.pflug@live.de>
2022-01-23 18:37:06 +0100

    VersionCompareData is now a record struct

--- Code/Synnotech.AspNetCore/VersionValidation/VersionCompareData.cs
+++ Code/Synnotech.AspNetCore/VersionValidation/VersionCompareData.cs
@@ -1,28 +1,8 @@
 ﻿namespace Synnotech.AspNetCore.VersionValidation;
 
 /// <summary>
-/// Struct containing data to compare the app version with a request version.
+/// Represents a structure containing data to compare the app version with a request version.
 /// </summary>
-public readonly struct VersionCompareData<T>
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="VersionCompareData{T}"/> struct.
-    /// </summary>
-    /// <param name="appVersion">The application version.</param>
-    /// <param name="requestVersion">The request version.</param>
-    public VersionCompareData(T appVersion, T requestVersion)
-    {
-        AppVersion = appVersion;
-        RequestVersion = requestVersion;
-    }
-
-    /// <summary>
-    /// Gets the application version.
-    /// </summary>
-    public T AppVersion { get; }
-
-    /// <summary>
-    /// Gets the request version.
-    /// </summary>
-    public T RequestVersion { get; }
-}
+/// <param name="AppVersion">The version of the app.</param>
+/// <param name="RequestVersion">The version of the request.</param>
+public readonly record struct VersionCompareData<T>(T AppVersion, T RequestVersion);
